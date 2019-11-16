7c12d821612d17956e6450d095489069ec7d8681
Luke Horne <luke.horne@live.co.uk>
2019-11-16 18:50:07 +0000

    Adds untested attempt at real world positioning (no phone available to run code)

--- js/ARnav.js
+++ js/ARnav.js
@@ -6,16 +6,36 @@ import { StyleSheet } from 'react-native';
 
 import { ViroARScene, ViroText, ViroConstants } from 'react-viro';
 
+import { latLonToMerc, arPosFromMercs, distanceToModel } from './utils/coords';
+
 export default class HelloWorldSceneAR extends Component {
   state = {
-    text: 'Initializing AR...'
+    currPos: [53.485979, -2.239815],
+    locations: [{ latLon: [53.486233, -2.241182], name: 'corner' }]
+  };
+
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        this.setState({ currPos: [latitude, longitude] });
+      },
+      console.log,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
   };
 
   render = () => {
     const { changePage } = this.props.sceneNavigator.viroAppProps;
+    const { locations, currPos } = this.state;
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
-        <ViroText text={this.state.text} scale={[50, 50, 50]} position={[0, 0, -100]} style={styles.helloWorldTextStyle} />
+        {locations.map(({ latLon, name }) => {
+          const objMercCoords = arPosFromMercs(latLon);
+          const currMercCoords = arPosFromMercs(currPos);
+          const arPos = arPosFromMercs(currMercCoords, objMercCoords);
+          const distance = distanceToModel(currMercCoords, objMercCoords);
+          <ViroText text={`${name} - ${distance}m`} scale={[50, 50, 50]} position={arPos} style={styles.helloWorldTextStyle} />;
+        })}
         <ViroText text="Back" scale={[50, 50, 50]} position={[0, -20, -100]} style={styles.helloWorldTextStyle} onClick={() => changePage('splash')} />
       </ViroARScene>
     );
