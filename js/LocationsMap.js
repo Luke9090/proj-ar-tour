e9648a2e031bc6f2e39463a11a033d578959f6f1
Chris Wray <chriswray76@gmail.com>
2019-11-18 15:38:50 +0000

    added comments

--- js/LocationsMap.js
+++ js/LocationsMap.js
@@ -24,17 +24,20 @@ export default class LocationsMap extends Component {
 
   render() {
     const { locations } = this.state;
-    // const mapHeight = Math.round(Dimensions.get('window').height * 0.33);
+    const mapHeight = Math.round(Dimensions.get('window').height * 0.33);
+    const mapWidth = Math.round(Dimensions.get('window').width);
     return (
       <ImageBackground style={{ height: 250, width: '100%' }} source={require('./res/Manchester-Map-Small.png')} >
-      <Text>{Dimensions.get('window').width}</Text>
         {locations.map(location => {
-          const latBase = 53.471965;
-          const longBase = 2.250271;
-          const latPixelRatio = 0.000030496;
-          const longPixelRatio = 0.00005408;
-          const latPixels = Math.round((location.coords._lat - latBase) / latPixelRatio);
-          const longPixels = Math.round((longBase + location.coords._long) /longPixelRatio);
+
+          const latBase = 53.471965; // latitude at bottom of map
+          const longBase = 2.250271; // longitude at left of map
+          const mapHeightInLat = 0.007624; // difference in latitude from top to bottom
+          const mapWidthInLong = 0.019947; // diference in longitude from left to right
+          const latPixelRatio = mapHeightInLat / mapHeight; // height in latitude divided by height in pixels
+          const longPixelRatio = mapWidthInLong / mapWidth; // width in longitude divided by width in pixels
+          const latPixels = Math.round((location.coords._lat - latBase) / latPixelRatio); // actual latitude minus base latitude divided by pixel ratio to give pixels from left
+          const longPixels = Math.round((longBase + location.coords._long) / longPixelRatio); // actual longitude minus base longitude divided by pixel ratio to give pixels from bottom
 
           const locationStyle = StyleSheet.create({
             location: {
@@ -46,7 +49,7 @@ export default class LocationsMap extends Component {
               bottom: latPixels
             }
           });
-          
+
           return (
             <View key={ location.name } style={ locationStyle.location }>
               <Image
@@ -81,7 +84,6 @@ const styles = StyleSheet.create({
     textAlign: 'center', 
     backgroundColor: 'rgba(0, 0, 0, 0.4)'
   }
-
 });
 
 module.exports = LocationsMap;
