e070b5954e99ee9c8e0813bf23ee110af6709464
Chris Wray <chriswray76@gmail.com>
2019-11-18 15:19:26 +0000

    implements geo-conversion to locate map pins

--- js/LocationsMap.js
+++ js/LocationsMap.js
@@ -27,28 +27,36 @@ export default class LocationsMap extends Component {
     // const mapHeight = Math.round(Dimensions.get('window').height * 0.33);
     return (
       <ImageBackground style={{ height: 250, width: '100%' }} source={require('./res/Manchester-Map-Small.png')} >
+      <Text>{Dimensions.get('window').width}</Text>
+        {locations.map(location => {
+          const latBase = 53.471965;
+          const longBase = 2.250271;
+          const latPixelRatio = 0.000030496;
+          const longPixelRatio = 0.00005408;
+          const latPixels = Math.round((location.coords._lat - latBase) / latPixelRatio);
+          const longPixels = Math.round((longBase + location.coords._long) /longPixelRatio);
 
-          {locations.map(location => {
-            const locationStyle = StyleSheet.create({
-              location: {
-                position: 'absolute',
-                display: 'flex',
-                flexDirection: 'row',
-                width: '100%',
-                left: location.left,
-                top: location.top
-              }
-            })
-            return (
-              <View key={ location.name } style={ locationStyle.location }>
-                <Image
-                  style={styles.mapPin}
-                  source={require('./res/mapPin.png')}
-                />
-                <Text style={styles.locationLabel}>{ location.name }</Text>
-              </View>
-            )
-          })}  
+          const locationStyle = StyleSheet.create({
+            location: {
+              position: 'absolute',
+              display: 'flex',
+              flexDirection: 'row',
+              width: '100%',
+              left: longPixels,
+              bottom: latPixels
+            }
+          });
+          
+          return (
+            <View key={ location.name } style={ locationStyle.location }>
+              <Image
+                style={styles.mapPin}
+                source={require('./res/mapPin.png')}
+              />
+              <Text style={styles.locationLabel}>{ location.name }</Text>
+            </View>
+          )
+        })}  
             
       </ImageBackground>
     );
