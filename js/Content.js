0b12546e20cda6ee1eb572a69e33e2b08a194ba1
Luke Horne <luke.horne@live.co.uk>
2019-11-20 17:31:59 +0000

    stops setting currLoc to null to avoid error

--- js/Content.js
+++ js/Content.js
@@ -31,7 +31,7 @@ export default class Content extends Component {
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              changePage('split', 'nav', 'arrival');
+              changePage('split', 'nav', 'arrival', currLoc);
             }}
           >
             <Text style={styles.buttonText}>Back</Text>
