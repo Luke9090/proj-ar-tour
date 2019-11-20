ffc5c93170439ce3760b134292b5c1c393fe5fcd
Ruth Sargent <ruthsargent@Ruths-MacBook-Pro.local>
2019-11-20 12:14:58 +0000

    add logo to splash screen

--- js/Splash.js
+++ js/Splash.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Image
+} from "react-native";
 
 function Splash({ changePage }) {
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.background} source={require('../imgs/concert-background.jpg')}>
-        <Text style={styles.header}>Manchest-AR</Text>
+      <ImageBackground
+        style={styles.background}
+        source={require("../imgs/concert-background.jpg")}
+      >
+        <Image style={styles.logo} source={require("../imgs/white-logo.png")} />
         <Text style={styles.text}>
-          Welcome to the music tour of Manchester. Point your phone along the pavement and hit 'Start Tour' to begin your journey.
+          Welcome to the music tour of Manchester. Point your phone along the
+          pavement and hit 'Start Tour' to begin your journey.
         </Text>
         <View style={styles.container2}>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              changePage('split', 'nav', 'map');
+              changePage("split", "nav", "map");
             }}
           >
             <Text style={styles.buttonText}>Start Tour</Text>
@@ -27,54 +38,55 @@ function Splash({ changePage }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center'
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
   map: {
     flex: 1
   },
   background: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
+    alignItems: "center"
+  },
+  logo: {
+    marginTop: 100
   },
   header: {
     flex: 2,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 40,
     paddingTop: 60,
     padding: 20,
-    color: 'white',
-    textShadowColor: 'black',
+    color: "white",
+    textShadowColor: "black",
     textShadowRadius: 2
-    // borderColor: "black",
-    // borderWidth: 1
   },
   text: {
-    flex: 2,
-    justifyContent: 'flex-start',
-    fontSize: 20,
-    padding: 40,
-    textAlign: 'center',
-    color: 'white',
-    textShadowColor: 'black',
-    textShadowRadius: 2
-    // borderColor: "black",
-    // borderWidth: 1
+    flex: 3,
+    justifyContent: "flex-start",
+    fontSize: 25,
+    padding: 50,
+    marginTop: 130,
+    textAlign: "center",
+    color: "white",
+    textShadowColor: "black",
+    textShadowRadius: 5
   },
   container2: {
     flex: 3,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "center",
     marginBottom: 50
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     margin: 10
   },
   buttonText: {
-    color: 'black',
+    color: "black",
     padding: 10
   }
 });
